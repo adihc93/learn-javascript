66c867d0608cd2eb0e9f5d3ce83f277485e112c5
adihc93 <chaturvedias93@gmail.com>
2022-07-25 17:09:39 +0530

    Assignment values and variables

--- complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -1,1 +1,10 @@
 alert('HI');
+
+//Values and Variables
+let country = 'India';
+let continent = 'Asia';
+let population = 1350;
+
+console.log(country);
+console.log(continent);
+console.log(population);
