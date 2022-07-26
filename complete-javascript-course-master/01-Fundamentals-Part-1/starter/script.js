a6ded046ea7bbdd4f2dd1a668b3e9babce7b0dd8
adihc93 <chaturvedias93@gmail.com>
2022-07-26 10:30:53 +0530

    Assignment data types

--- complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -9,4 +9,11 @@ console.log(country);
 console.log(continent);
 console.log(population);
 
+//Data Types
 let isIsland = false;
+let language;
+
+console.log(typeof isIsland);
+console.log(typeof country);
+console.log(typeof population);
+console.log(typeof language);
