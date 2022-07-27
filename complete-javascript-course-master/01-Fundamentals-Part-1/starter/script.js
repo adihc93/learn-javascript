b138a83f14029cedca0aafd13a1398d8b7542eb3
adihc93 <chaturvedias93@gmail.com>
2022-07-27 15:12:37 +0530

    assignment basic operator

--- complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -20,3 +20,18 @@ console.log(typeof language);
 
 //let, const and var
 language = 'Hindi'; 
+
+//Basic operator
+
+let halfPopulation = population/2;
+console.log(halfPopulation);
+console.log(halfPopulation+1);
+
+let finlandPopulation = 60;
+console.log(population > finlandPopulation);
+
+let averagePoplulation = 33;
+console.log(population < averagePoplulation);
+
+let description = country+' is in '+continent+', and its '+population+' million people speaks '+language;
+console.log(description);
