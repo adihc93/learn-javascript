581b2f785bbf151939a5e7823c762b114e8241a8
adihc93 <chaturvedias93@gmail.com>
2022-07-27 16:41:37 +0530

    coding challenge #1

--- complete-javascript-course-master/01-Fundamentals-Part-1/coding challenges/script.js
+++ complete-javascript-course-master/01-Fundamentals-Part-1/coding challenges/script.js
@@ -1,1 +1,15 @@
 alert('Coding Challenges');
+
+//Coding challenge #1
+let massMark = 78;
+let heightMark = 1.69;
+
+let massJohn = 92;
+let heightJohn = 1.95;
+
+let markBMI = massMark/(heightMark**2);
+let johnBMI = massJohn/(heightJohn**2);
+
+let markHigherBMI = markBMI > johnBMI;
+
+console.log(markBMI, johnBMI, markHigherBMI);
