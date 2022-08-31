7b9b0ffde5d96087e5d5562fe27eca532c43d32a
adihc93 <35630730+adihc93@users.noreply.github.com>
2022-08-31 17:05:03 +0530

    coding challenge #3

--- complete-javascript-course-master/01-Fundamentals-Part-1/coding challenges/script.js
+++ complete-javascript-course-master/01-Fundamentals-Part-1/coding challenges/script.js
@@ -1,6 +1,4 @@
-alert('Coding Challenges');
-
-//Coding challenge #1
+/*//Coding challenge #1
 let massMark = 78;
 let heightMark = 1.69;
 
@@ -17,4 +15,23 @@ if (markHigherBMI) {
     console.log(`Marks's BMI (${markBMI}) is higher than John's BMI (${johnBMI})`);
 } else {
     console.log(`John's BMI (${johnBMI}) is higher than Mark's BMI (${markBMI})`);
+}*/
+
+//Coding Challenge #3
+const teamDolphins1 = 96, teamDolphins2 = 108, teamDolphins3 = 89;
+const teamKoalas1 = 88, teamKoalas2 = 91, teamKoalas3 = 110;
+const teamDolphinsAvg = (teamDolphins1 + teamDolphins2 + teamDolphins3) / 3;
+const teamKoalasAvg = (teamKoalas1 + teamKoalas2 + teamKoalas3) / 3;
+
+if (teamDolphinsAvg > teamKoalasAvg && teamDolphinsAvg >= 100) {
+    console.log('Dolphins wins');
+}
+else if (teamKoalasAvg > teamDolphinsAvg && teamKoalasAvg >= 100) {
+    console.log('Koalas wins');
+}
+else if (teamDolphinsAvg === teamKoalasAvg && teamDolphinsAvg >= 100 && teamKoalasAvg >= 100) {
+    console.log('Its draw');
+}
+else {
+    console.log('No one wins');
 }
